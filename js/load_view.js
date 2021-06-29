0712e5f6d3cbb82483a53581de6d3af49317b4f3
Jonathan Lorentz <lorentzjonathan108@gmail.com>
2021-06-29 16:09:37 -0400

    initial

---
+++ js/load_view.js
@@ -0,0 +1,5 @@
+"use strict";
+function load_view() {
+    console.log("test");
+}
+document.body.onload = load_view;
---
+++ ts/load_view.ts
@@ -0,0 +1,5 @@
+function load_view() {
+    console.log("test");
+}
+
+document.body.onload = load_view;
